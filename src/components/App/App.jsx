1469d01c3b4bbb505dee2c641c0e0f16edddbdbd
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2025-07-10 21:34:11 +0300

    start components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notifications from "../Notifications/Notifications";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [values, setValues] = useState(() => {
+    const savedFeedbacks = window.localStorage.getItem("savedFeedbacks");
+    return savedFeedbacks !== null
+      ? JSON.parse(savedFeedbacks)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setValues({ ...values, [feedbackType]: values[feedbackType] + 1 });
+  };
+
+  const clearFeedback = () => {
+    setValues({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  let totalFeedback = values.good + values.neutral + values.bad;
+
+  useEffect(() => {
+    window.localStorage.setItem("savedFeedbacks", JSON.stringify(values));
+  }, [values]);
 
   return (
     <>
-
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        reset={clearFeedback}
+      />
+      {totalFeedback === 0 ? (
+        <Notifications />
+      ) : (
+        <Feedback
+          good={values.good}
+          neutral={values.neutral}
+          bad={values.bad}
+          total={totalFeedback}
+        />
+      )}
     </>
-  )
+  );
 }
-
-export default App
